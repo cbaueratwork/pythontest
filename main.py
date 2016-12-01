293bd26205bb8c51c3f11ba0abfa9ed2fbf82e7d
Chris Bauer <bauerpower@google.com>
2016-12-01 14:17:39 -0800

    formatting

--- main.py
+++ main.py
@@ -27,18 +27,18 @@ class TakeSomeTime(webapp2.RequestHandler):
     wait_time = random.random()
     time.sleep(wait_time)
     self.response.headers['Content-Type'] = 'text/plain'
-    self.response.write('Waited for {}ms before returning'.format(wait_time))
+    self.response.write('Waited for {}ms before returning'.format(wait_time * 1000))
 
 
 class PossibleException(webapp2.RequestHandler):
   def get(self):
     divisor = random.randint(0, 10)
     self.response.headers['Content-Type'] = 'text/plain'
-  try:
-    answer = 42 / divisor
-    self.response.write('Divided 42 by {}, got {}'.format(divisor, answer))
-  except ZeroDivisionError:
-    self.response.write('Error: attempted to divide by zero')
+    try:
+      answer = 42 / divisor
+      self.response.write('Divided 42 by {}, got {}'.format(divisor, answer))
+    except ZeroDivisionError:
+      self.response.write('Error: attempted to divide by zero')
 
 
 
